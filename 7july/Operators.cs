b62a640fcec401a81c837aed339c030c87abd110
poonamrajkumarpise <punampise5@gmail.com>
2026-07-08 22:39:25 +0530

    Initial commit

---
+++ 7july/Operators.cs
@@ -0,0 +1,41 @@
+
+/*
+public class Operators
+{
+    static void Main()
+    {
+    
+//arithmetic operators//
+int a=10;
+int b=20;
+Console.WriteLine("add is"+(a+b));
+Console.WriteLine("mul is"+(a*b));
+Console.WriteLine("sub is"+(a-b));
+Console.WriteLine("div is"+(a%b));
+
+//logical operators//
+bool val1=true;  bool val2=false;
+Console.WriteLine(val1&&val2);
+Console.WriteLine(val1||val2);
+
+// assignment operators//
+int x=1; int y=2; 
+Console.WriteLine(x+=2);
+Console.WriteLine(y+=2);
+Console.WriteLine(x-=1);
+
+//relational operators//
+int num1=30; int num2=40; 
+Console.WriteLine(num1>num2);
+Console.WriteLine(num1<num2);
+Console.WriteLine(num1!=num2);
+
+
+
+
+    }
+
+
+
+}
+*/
---
+++ 8july/Classobjecteg.cs
@@ -0,0 +1,11 @@
+﻿/*
+class Classobjecteg
+{
+static void Main()
+{
+    Student student=new Student(22,"poonam","ssgmce",2882007,'F',2.6f);
+    student.display();
+}
+
+}
+*/
